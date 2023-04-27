c1e8f02eadbd4c7be5c3517ffb561fbdda3f217a
Leandrito12 <leandroandresrueda@gmail.com>
2023-04-27 09:57:52 -0300

    borrado de codigo inutil

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header'
-import Carousel from './components/Carousel'
-import Home from './pages/Home';
-import { Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
-import City from './pages/Cities';
-import ButtonCTA from './components/ButtonCTA';
-
-
+import "./App.css";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import City from "./pages/Cities";
 
 function App() {
-  
   return (
     <>
-
-          <Routes>    {/* acorde a la ruta */}
-            <Route  path={'/'} element={<Home/>}  /> 
-            <Route  path={'/cities'} element={<City/>}  /> 
-            <Route  path={'*'} element={<h1> No funcaaa </h1>} /> 
-          </Routes>
-
-       <Footer/>   {/* fijo */}
+      <Routes>
+        {" "}
+        {/* acorde a la ruta */}
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/cities"} element={<City />} />
+        <Route path={"*"} element={<h1> No funcaaa </h1>} />
+      </Routes>
+      <Footer /> {/* fijo */}
     </>
   );
 }
