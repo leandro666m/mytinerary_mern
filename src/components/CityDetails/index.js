66cfe2dc2243d4880ee6b34c383a78b59d5a3eee
Leandrito12 <leandroandresrueda@gmail.com>
2023-05-11 09:56:54 -0300

    mejoras en el css de itinerary

--- src/components/CityDetails/index.js
+++ src/components/CityDetails/index.js
@@ -2,63 +2,73 @@ import React, { useEffect, useState } from "react";
 import "./styles/cityDetails.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import ButtonCTA from '../ButtonCTA'
+import ButtonCTA from "../ButtonCTA";
 import { filterCityByID } from "../../redux/slices/city/citySlice";
 
-
 export default function CityDetails() {
-  const { id } = useParams(); 
-  const dispatch = useDispatch()
-  
-  let cities = useSelector( (store)=> store.cities.data ) 
-  let city = useSelector( (store)=> store.cities.dataFiltered )
-  
-  useEffect( ()=>{
-    if( cities.length > 0){ 
-      dispatch( filterCityByID(id) ) }
-    }, [cities] ) 
-    console.log(city);
-    
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  let cities = useSelector((store) => store.cities.data);
+  let city = useSelector((store) => store.cities.dataFiltered);
+
+  useEffect(() => {
+    if (cities.length > 0) {
+      dispatch(filterCityByID(id));
+    }
+  }, [cities]);
+  console.log(city);
+
   return (
-    <> { city ?
-      <section className="card-back-details container-fluid d-flex justify-content-around flex-wrap">
-        <div className="card-details ">
-          <div className="img-details-container">
-            <img src={city.imageURL} alt={city.name} className="img-details" />
-          </div>
-          <div className="card-description">
-            <h3>{city.name}</h3>
-            <p className="description">{city.description}</p>
-          </div>
-          { city?.itineraries?.length !== 0 ?
-            <div className="container-itineraries">
-              {city.itineraries?.map((iti) => (
-                <div className="itinerary" key={iti._id} >
-                  <img src={iti.photoAutor} className="photoAutor" />
-                  <p>{iti.nameItinerary}  </p>
-                  <p>Autor: {iti.nameAutor}  </p>
-                  <p>Value: ${iti.precio}  </p>
-                  <p>Duration: {iti.duracion} dias</p>
-                  <p><i class="fa-solid fa-heart"></i>{iti.likes}</p>
-
-                  {
-                    iti.hashtags.length !== 0 &&
-                    <div className="hashtags">
-                      {iti.hashtags.map((ht) => (<span key={ht}>{ht} </span>))}
-                    </div>
-                  }
-                </div>
-              ))}
+    <>
+      {" "}
+      {city ? (
+        <section className="card-back-details container-fluid d-flex justify-content-around flex-wrap">
+          <div className="card-details w-100">
+            <div className="img-details-container">
+              <img
+                src={city.imageURL}
+                alt={city.name}
+                className="img-details"
+              />
             </div>
-            : " "}
-          <div className="hotels">
+            <div className="card-description rounded opacity-75">
+              <h3>{city.name}</h3>
+              <p className="description">{city.description}</p>
+            </div>
+            {city?.itineraries?.length !== 0 ? (
+              <div className="container-itineraries">
+                {city.itineraries?.map((iti) => (
+                  <div className="itinerary" key={iti._id}>
+                    <img src={iti.photoAutor} className="photoAutor" />
+                    <p className="text">{iti.nameItinerary} </p>
+                    <p>Autor: {iti.nameAutor} </p>
+                    <p>Value: ${iti.precio} </p>
+                    <p>Duration: {iti.duracion} dias</p>
+                    <p>
+                      <i class="fa-solid fa-heart"></i>
+                      {iti.likes}
+                    </p>
 
+                    {iti.hashtags.length !== 0 && (
+                      <div className="hashtags">
+                        {iti.hashtags.map((ht) => (
+                          <span key={ht}>{ht} </span>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            ) : (
+              " "
+            )}
+            <div className="hotels"></div>
+
+            <ButtonCTA buttonText="Back to explore" buttonLink="/cities" />
           </div>
-
-          <ButtonCTA buttonText="Back to explore" buttonLink='/cities' />
-        </div>
-      </section>
-: null}
-</>
+        </section>
+      ) : null}
+    </>
   );
 }
